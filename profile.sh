e62af6338d7edbd4166311db4a4f8b1564dd11e4
Johannes Rosenberger <jo.rosenberger@gmx-topmail.de>
2016-05-20 00:36:52 +0200

    Cleaned profile.sh.

--- profile.sh
+++ profile.sh
@@ -1,10 +1,8 @@
-# To avoid potential situation where cdm(1) crashes on every TTY, here we
-# default to execute cdm(1) on tty1 only, and leave other TTYs untouched.
+#!/bin/sh
+
 if [[ "$EUID" != 0 ]]; then
 	if [[ "$(tty)" == /dev/tty* ]]; then
 		[[ -n "$SCDM_SPAWN" ]] && return
-		# Avoid executing cdm(1) when X11 has already been started.
-		#[[ -z "$DISPLAY$SSH_TTY$(pgrep xinit)" ]] && exec cdm
 		[[ -z "$SSH_TTY" ]] && exec scdm
 	fi
 fi
