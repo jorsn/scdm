a6c086305f8f3858ecfd4af744307a695578c09e
Johannes Rosenberger <johannes.rosenberger@jorsn.eu>
2018-06-18 01:32:29 +0200

    removed rebasing from nix patch generation

--- nix/gen-nixos-man-patch.sh
+++ nix/gen-nixos-man-patch.sh
@@ -4,8 +4,4 @@ set -e
 
 cd "$(dirname "$0")"
 
-man_branch=man-nixos
-branch="$(git branch | sed -n 's/^\* //p')"
-git rebase -q "$branch" "$man_branch"
-git checkout -q "$branch"
-git diff .."$man_branch" | sed '1,2d' > nixos-man.patch
+git diff ..man-nixos | sed '1,2d' > nixos-man.patch
